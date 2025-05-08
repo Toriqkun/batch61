219473be7063228eb4159d2f5fdf3a6f3067b44a
toriqkun25 <toriqkun25@gmail.com>
2025-05-08 14:48:59 +0700

    Tugas hari ke 5

--- task4/contact.js
+++ task4/contact.js
@@ -10,18 +10,39 @@ toggleBtn.onclick = function () {
 
 // ini form contact
 
-let myname = "toriq rosid";
-let myemail = "toriqrosid@example.com";
-let mynumber = "08111111111";
-let subject = "saran";
-let message = "halo world";
+// let myname = "toriq rosid";
+// let myemail = "toriqrosid@example.com";
+// let mynumber = "08111111111";
+// let subject = "saran";
+// let message = "halo world";
 
-function contactme() {
-  console.log("Nama: " + myname);
-  console.log("Email: " + myemail);
-  console.log("Number:" + mynumber);
-  console.log("Subject: " + subject);
-  console.log("Your Message" + message);
+// function contactme() {
+//   console.log("Nama: " + myname);
+//   console.log("Email: " + myemail);
+//   console.log("Number:" + mynumber);
+//   console.log("Subject: " + subject);
+//   console.log("Your Message: " + message);
+// }
+
+// contactme();
+
+function contactme(e) {
+  e.preventDefault();
+
+  let name = document.GetElementById("name").value;
+  let email = document.GetElementById("email").value;
+  let number = document.GetElementById("number").value;
+  let subject = document.GetElementById("select").value;
+  let textarea = document.GetElementById("textarea").value;
+
+  console.log(`Nama: ${name}
+               Email: ${email}
+               Phone Number: ${number}
+               Subject: ${subject}
+               Your Message: ${textarea}`);
+
+  alert();
+  changeElement();
 }
 
 contactme();
